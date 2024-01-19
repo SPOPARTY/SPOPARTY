a585ff880d0368a054d2408b2d4393f560ba5827
김종범 <kbumk1234@naver.com>
2024-01-19 14:10:17 +0900

    chore: #Jira_properties->yml,private.yml_S10P12A802-132
    1. application.properties를 application.yml로 변환
    2. datasource 정보를 apllication-private.yml에 등록 후 gitignore에 등록.
    3. com.spoparty로 디렉토리 이름 수정

---
+++ backend/src/main/java/com/spoparty/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.spoparty.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+/**
+ * API 문서 관련 swagger2 설정 정의.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
