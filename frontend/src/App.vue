766e8b4b0ffe219308110529d8f54ea59ea0245d
황인규 <rlawldbs42@gmail.com>
2024-01-22 01:34:33 +0900

    vuetify적용

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,26 +4,18 @@ import HeaderNav from "./components/common/HeaderNav.vue";
 import Footer from "./components/common/Footer.vue";
 </script>
 <template>
-  <div class="header-nav">
+  <v-app>
+  <div>
     <HeaderNav/>
     </div>
-    <div class="main-app">
-    <RouterView/>
-    </div>
+    <v-main class="mt-1">
+      <RouterView/>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
-.header-nav {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  background-color: #c7c7c7;
-}
 
-.main-app {
-  margin: 20px 30px 0px 30px;
-  
-}
+
 
 </style>
--- frontend/src/views/MainPageView.vue
+++ frontend/src/views/MainPageView.vue
@@ -1,63 +1,37 @@
 <template>
-     <div class="main-container">
-          <div class="club-list">
-               <GroupNav/>
-               <div class="test">
-                    <h1>Test 2048px</h1>
-               </div>
-          </div>
-          <div class="main-contents">
-               <div class="main-contents-1">
-                    <CheerVote/>
-               </div>
-               <div class="main-contents-2">
-                    <MatchNear/>
-               </div>
-               <div class="test">
-                    <h1>Test 2048px</h1>
-               </div>
-          </div>
-     </div>
-</template>
+     <v-app>
+          <v-container fluid>
+               <!-- 메인 컨텐츠 영역 -->
+               <v-row>
+                    <!-- 첫 번째 컨텐츠 영역: 응원 투표 -->
+                    <v-col cols="12" class="my-1">
+                         <CheerVote />
+                    </v-col>
 
+                    <!-- 두 번째 컨텐츠 영역: 경기 일정 -->
+                    <v-col cols="12" class="my-1">
+                         <MatchNear />
+                    </v-col>
+               </v-row>
+
+               <!-- 테스트 영역 -->
+               <!-- <v-row>
+                    <v-col cols="12">
+                         <div class="test">
+                              <h1>Test 2048px</h1>
+                         </div>
+                    </v-col>
+               </v-row> -->
+          </v-container>
+     </v-app>
+</template>
+   
 <script setup>
-import GroupNav from "@/components/common/GroupNav.vue";
 import MatchNear from "@/components/mainpage/MatchNear.vue";
 import CheerVote from "@/components/mainpage/CheerVote.vue";
-
 </script>
-
-<style>
-.test {
-     height: 2048px;
-}
-
-.main-container {
-     display: flex;
-     flex-direction: row;
-     padding: 10px;
-}
-
-.club-list {
-     position: fixed;
-     top: 30px;
-     left: 40px; /* 오른쪽 마진 + 패딩에 해당하는 값만큼 */
-     width: 255px;
-     height: 100%;
-     overflow-y: auto; /* 세로 스크롤을 사용하기 위한 설정 */
-     background-color: grey;
-}
-
-.main-contents {
-     width: 100%;
-     height: 100%;
-     background-color: skyblue;
-     padding: 0px 0px 10px 0px;
-     margin-left: 270px;
-}
-
-.main-contents-1 {
-     margin-bottom: 20px;
-}
+   
+<style scoped>
 
 </style>
+   
