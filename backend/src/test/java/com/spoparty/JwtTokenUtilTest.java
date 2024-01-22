0bd5626961cd5ee51e38db660a3b5cadc36d9e16
이효리 <gyfl5542@naver.com>
2024-01-22 12:26:14 +0900

    chore: 테스트 설정 -  gradle.build 수정 및 test 디렉토리 설정

---
+++ backend/src/test/java/com/spoparty/JwtTokenUtilTest.java
@@ -0,0 +1,13 @@
+package com.spoparty;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+
+
+@RunWith(SpringRunner.class)
+// 테스트임을 나타냄.
+@SpringBootTest
+public class JwtTokenUtilTest {
+
+}
