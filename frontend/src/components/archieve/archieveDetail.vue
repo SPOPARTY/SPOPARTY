aba1903ddd2ce6c9888038e4be337fab1c12436e
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-01-24 09:15:29 +0900

    fix:archieve수정

--- frontend/src/components/archieve/archieveDetail.vue
+++ frontend/src/components/archieve/archieveDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>추억 하나</h1>
-        
+        <h2>추억 둘</h2>
     </div>
 </template>
 
--- frontend/src/components/archieve/archieveList.vue
+++ frontend/src/components/archieve/archieveList.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>여기는 추억 저장소</h1>
         <h2>경로 : {{ routes.params.clubId }}</h2>
+        <h3>게시글 리스트</h3>
     </div>
 </template>
 
