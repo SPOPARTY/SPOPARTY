17b80650bb39456ea57cf5511c76284b1d5493b8
김종범 <kbumk1234@naver.com>
2024-01-25 14:34:25 +0900

    style: 충돌수정

--- backend/src/main/java/com/spoparty/api/football/repository/TeamRepository.java
+++ backend/src/main/java/com/spoparty/api/football/repository/TeamRepository.java
@@ -1,12 +1,15 @@
 package com.spoparty.api.football.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 
 import com.spoparty.api.football.entity.Team;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+	<T> Optional<T> findById(Long id, Class<T> type);
 
 }
