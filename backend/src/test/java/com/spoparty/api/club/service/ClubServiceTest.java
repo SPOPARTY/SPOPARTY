cf1986ebab8ad5c3d0fed4981c4c53500ce222b1
조은별 <joeunbyeol98@gmail.com>
2024-01-25 17:52:52 +0900

    test: ClubService 테스트 클래스 생성

---
+++ backend/src/test/java/com/spoparty/api/club/service/ClubServiceTest.java
@@ -0,0 +1,50 @@
+package com.spoparty.api.club.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class ClubServiceTest {
+
+	@Test
+	void createClub() {
+
+	}
+
+	// @Test
+	// void findRecentClubs() {
+	// }
+	//
+	// @Test
+	// void updateClubName() {
+	// }
+	//
+	// @Test
+	// void deleteClub() {
+	// }
+	//
+	// @Test
+	// void getInviteUrl() {
+	// }
+	//
+	// @Test
+	// void getGroupMembers() {
+	// }
+	//
+	// @Test
+	// void createGroupMember() {
+	// }
+	//
+	// @Test
+	// void assignHost() {
+	// }
+	//
+	// @Test
+	// void deleteGroupMember() {
+	// }
+}
