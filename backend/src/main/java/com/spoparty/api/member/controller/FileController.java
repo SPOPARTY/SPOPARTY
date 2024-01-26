ee40f8c6c7e90195e6afc1b760c8600768a15171
김종범 <kbumk1234@naver.com>
2024-01-26 23:50:07 +0900

    feat: board CRUD
    club table이 이미 있어서 엔티티가 있다고 가정하고 주석처리하고 구현
    file upload 로직 service로 분할

--- backend/src/main/java/com/spoparty/api/member/controller/FileController.java
+++ backend/src/main/java/com/spoparty/api/member/controller/FileController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.spoparty.api.member.entity.File;
-import com.spoparty.api.member.repository.FileRepository;
+import com.spoparty.api.member.service.FileService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,42 +25,24 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/files")
 public class FileController {
 
-	private final AmazonS3 amazonS3;
-	private final FileRepository fileRepository;
-
-	@Value("${cloud.aws.s3.bucket}")
-	private String bucket;
-
-	@Value("${cloud.aws.region.static}")
-	private String region;
+	private final FileService fileService;
 
 	@PostMapping("/upload")
 	public ResponseEntity<?> uploadFiles(@RequestParam("files") MultipartFile[] files) {
 		List<File> list = new ArrayList<>();
 		try {
 			for (MultipartFile file : files) {
-				String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-				ObjectMetadata metadata = new ObjectMetadata();
-				metadata.setContentType(file.getContentType());
-				metadata.setContentLength(file.getSize());
-				amazonS3.putObject(bucket, fileName, file.getInputStream(), metadata);
-
-				File saveFile = new File();
-				saveFile.setType("image");
-				saveFile.setUrl("https://" + bucket + ".s3." + region + ".amazonaws.com/" + fileName);
-				list.add(saveFile);
+				list.add(fileService.uploadFile(file));
 			}
-			fileRepository.saveAll(list);
 			return ResponseEntity.status(201).body(list);
 		} catch (IOException e) {
-			log.error(e.getMessage());
 			return ResponseEntity.status(400).body(null);
 		}
 	}
 
 	@GetMapping("/{type}")
 	public ResponseEntity<?> findFileByType(@PathVariable("type") String type) {
-		List<File> list = fileRepository.findByType(type, File.class);
+		List<File> list = fileService.findByType(type);
 		return ResponseEntity.status(200).body(list);
 	}
 
