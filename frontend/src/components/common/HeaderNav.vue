c851f59e87b47f45269bf84cdc380b3561546d14
황인규 <rlawldbs42@gmail.com>
2024-01-27 11:54:18 +0900

    [Style] 새 팔레트 적용 시험 + 컴포넌트 수정

--- frontend/src/components/common/HeaderNav.vue
+++ frontend/src/components/common/HeaderNav.vue
@@ -1,7 +1,7 @@
 <template>
   <v-navigation-drawer class="bar-border" v-model="drawer" app permanent color="white">
     <!-- 여기 color 옵션에 따라 자동으로 이후 텍스트 컬러도 결정됨 -->
-  <!-- <v-navigation-drawer class="bar-border" v-model="drawer" app permanent color="grey darken-3"> -->
+    <!-- <v-navigation-drawer class="bar-border" v-model="drawer" app permanent color="grey darken-3"> -->
     <!-- 네비게이션 타이틀 굵은 글씨로 -->
     <v-list-item class="py-2 club-title">
       <div>클럽 목록</div>
@@ -26,20 +26,24 @@
     </v-list>
 
     <!-- 로그인 관련 footer -->
-    <v-list-item class="sidebar-footer">
-      <v-icon class="footer-icon">
-        mdi-account-circle
-      </v-icon>
+    <v-list-item class="sidebar-footer" align="center">
+      <v-btn text to="/signup" class="mx-2" color="primary">
+        <v-icon size="x-large">mdi-account-plus</v-icon>
+      </v-btn>
+
+      <v-btn text to="/login" class="mx-2" color="primary">
+        <v-icon size="x-large">mdi-login</v-icon>
+      </v-btn>
     </v-list-item>
   </v-navigation-drawer>
 
-  <v-app-bar class="bar-border" app color="#000000" :elevation="2" rounded>
+  <v-app-bar class="bar-border" app color="#000000" :elevation="1">
     <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     <v-divider vertical class="mx-2"></v-divider>
 
     <!-- 로고 이미지 영역 -->
     <div class="logo-container" @click="goHome" style="cursor: pointer; flex: none; width: 240px;">
-      <v-img src="src/assets/spoparty-logo.png" contain></v-img>
+      <v-img src="src/assets/new-logo-2.png" contain></v-img>
     </div>
 
     <!-- 로고와 나머지 요소들 사이에 v-spacer 배치 -->
@@ -78,6 +82,10 @@ const clubs = ref([
   { id: 6, name: '클럽 BB' },
   { id: 7, name: '클럽 CC' },
   { id: 8, name: '클럽 DD' },
+  { id: 9, name: '클럽 E' },
+  { id: 10, name: '클럽 F' },
+  { id: 11, name: '클럽 G' },
+  { id: 12, name: '클럽 H' },
 ]);
 
 // 클럽 페이지를 새 탭에서 열기
@@ -91,6 +99,7 @@ function openClubInNewTab(clubId) {
 function goToNewClubPage() {
   router.push('/new-club'); // '새 클럽' 페이지로 라우팅하는 경로를 적절히 조정하세요.
 }
+
 </script>
 
 <style scoped>
@@ -101,14 +110,16 @@ function goToNewClubPage() {
   width: 240px;
   /* 로고 컨테이너의 너비 고정 */
 }
+
 .club-title {
   background-color: #08042B;
-  border : 2px solid #292071;
+  border: 2px solid #292071;
   height: 64px;
   font-size: 2rem;
   color: white;
   padding: 2rem;
 }
+
 span.v-btn__content {
   font-size: 2rem;
 }
@@ -139,9 +150,9 @@ span.v-btn__content {
 
 .club-item {
   width: 100%;
-  background-color: lightgray;
+  background-color: #CBD0D8;
   margin-bottom: 10px;
-  border-radius: 4px;
+  border-radius: 8px;
   /* 모서리를 약간 둥글게 */
   box-shadow: 0 2px 4px rgba(0, 0, 0, .1);
   /* 약간의 그림자 효과 */
@@ -153,7 +164,22 @@ span.v-btn__content {
 
 .sidebar-footer {
   height: 64px;
-  background-color: #292071;
+  background-color: #333D51;
+}
+
+/* Chrome, Edge, Safari */
+.club-list::-webkit-scrollbar {
+  display: none; /* 스크롤바 영역을 숨깁니다 */
+}
+
+/* Firefox */
+.club-list {
+  scrollbar-width: none; /* Firefox에서 스크롤바를 숨깁니다 */
+}
+
+/* IE and Edge */
+.club-list {
+  -ms-overflow-style: none; /* Internet Explorer 및 Edge에서 스크롤바를 숨깁니다 */
 }
 
 </style>
