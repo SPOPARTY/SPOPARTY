9b92930675fcaff8423618734b9f3eaeeb40f32f
eun-byeol <joeunbyeol98@gmail.com>
2024-01-29 02:12:58 +0900

    feat: 그룹 관련 crud 서비스 로직 구현

--- backend/src/main/java/com/spoparty/api/club/service/ClubService.java
+++ backend/src/main/java/com/spoparty/api/club/service/ClubService.java
@@ -2,85 +2,32 @@ package com.spoparty.api.club.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.spoparty.api.club.dto.ClubHostRequestDto;
 import com.spoparty.api.club.dto.ClubMemberResponseDto;
 import com.spoparty.api.club.dto.ClubRequestDto;
 import com.spoparty.api.club.dto.ClubResponseDto;
 import com.spoparty.api.club.dto.InviteRequestDto;
 import com.spoparty.api.club.dto.InviteResponseDto;
-import com.spoparty.api.club.entity.Club;
-import com.spoparty.api.club.entity.ClubMember;
-import com.spoparty.api.club.repository.ClubMemberRepository;
-import com.spoparty.api.club.repository.ClubRepository;
-import com.spoparty.api.common.entity.RoleType;
 import com.spoparty.api.member.entity.Member;
-import com.spoparty.api.member.repository.MemberRepository;
-import com.spoparty.api.party.repository.PartyRepository;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+public interface ClubService {
+	List<ClubResponseDto> findRecentClubs(Member member);
 
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-@Slf4j
-public class ClubService {
-	private final ClubRepository clubRepository;
-	private final ClubMemberRepository clubMemberRepository;
-	private final PartyRepository partyRepository;
-	private final MemberRepository memberRepository;
+	ClubResponseDto createClub(ClubRequestDto clubRequestDto);
 
-	@Transactional
-	public long createClub(ClubRequestDto clubRequestDto) {
-		Member member = memberRepository.findByMemberId(clubRequestDto.getMemberId());
-		Club club = Club.createClub(clubRequestDto.getName());
-		clubRepository.save(club);
+	ClubResponseDto findClub(Long clubId);
 
-		// 클럽회원 생성
-		// 이미 존재하는 멤버인지 확인하는 로직 필요
-		ClubMember clubMember = ClubMember.createClubMember(member, club, RoleType.host);
-		club.addClubMember(clubMember);
-		clubMemberRepository.save(clubMember);
+	ClubResponseDto updateClubName(Member member, Long clubId, ClubRequestDto clubRequestDto);
 
-		// log.debug("Member 조회 - {}", member);
-		// log.debug("ClubMember 조회 - {}", clubMember);
-		return club.getId();
-	}
+	Long deleteClub(Member member, Long clubId);
 
-	public List<ClubResponseDto> findRecentClubs(long memberId) {
-		Member member = memberRepository.findByMemberId(memberId);
-		
-		return null;
-	}
+	InviteResponseDto getInviteUrl(Long clubId);
 
-	public Boolean updateClubName(long clubId, ClubRequestDto clubRequestDto) {
-		return null;
-	}
+	ClubMemberResponseDto createGroupMember(InviteRequestDto inviteRequestDto);
 
-	public Boolean deleteClub(long clubId) {
-		return null;
-	}
+	List<ClubMemberResponseDto> getGroupMembers(Long clubId);
 
-	public InviteResponseDto getInviteUrl(long clubId) {
-		return null;
-	}
+	ClubMemberResponseDto assignHost(Member hostMember, Long clubId, ClubHostRequestDto clubHostRequestDto);
 
-	public List<ClubMemberResponseDto> getGroupMembers(long clubId) {
-		return null;
-	}
-
-	public Boolean createGroupMember(long clubId, InviteRequestDto inviteRequestDto) {
-		return null;
-	}
-
-	public Boolean assignHost(long clubId, ClubHostRequestDto clubHostRequestDto) {
-		return null;
-	}
-
-	public Boolean deleteGroupMember(long clubId, long memberId) {
-		return null;
-	}
+	Long deleteGroupMember(Member member, Long clubId);
 }
