d26764e7be990136ec35e68b74b0d0a3c929fe57
김종범 <kbumk1234@naver.com>
2024-01-29 14:38:36 +0900

    feat: 게시글 기능 정상화

--- backend/src/main/java/com/spoparty/api/board/repository/BoardRepository.java
+++ backend/src/main/java/com/spoparty/api/board/repository/BoardRepository.java
@@ -1,5 +1,6 @@
 package com.spoparty.api.board.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +9,7 @@ import com.spoparty.api.board.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-	// <T> List<T> findByClub_id(Long clubId, Class<T> type);
+	<T> List<T> findByClub_id(Long clubId, Class<T> type);
 
 	<T> Optional<T> findById(Long id, Class<T> type);
 
