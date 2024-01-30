d012d73290bcc61e2ea761ee430c20680c855eca
장승호 <jason9865@naver.com>
2024-01-30 20:55:09 +0900

    [Feat] 그룹 기능 및 그룹 게시판 Store

--- frontend/src/api/board.js
+++ frontend/src/api/board.js
@@ -4,22 +4,27 @@ const local = localAxios();
 
 const API_URL = "/boards"
 
+// 게시판에 있는 게시물 전체 조회
 function requestBoardList(clubId,success,fail) {
     local.get(`${API_URL}/clubs/${clubId}`).then(success).catch(fail);
 }
 
+// 게시글 상세 조회
 function requestSearchBoard(boardId, success, fail) {
     local.get(`${API_URL}/${boardId}`).then(success).catch(fail);
 }
 
+// 게시글 작성
 function requestCreateBoard(data,success,fail) {
     local.post("/boards",data).then(success).catch(fail);
 }
 
+// 게시글 수정
 function requestUpdateBoard(data,success,fail) {
     local.put("/boards",data).then(success).catch(fail);
 }
 
+// 게시글 삭제
 function requestDeleteBoard(boardId,success, fail) {
     local.delete(`${API_URL}/${boardId}`).then(success).catch(fail);
 }
--- frontend/src/stores/club/boards.js
+++ frontend/src/stores/club/boards.js
@@ -8,10 +8,101 @@ import {requestBoardList, requestSearchBoard, requestCreateBoard, requestUpdateB
 
 export const useBoardStore = defineStore("board",() => {
 
+    const boardList = ref(null);
+    const boardDetail = ref(null);
 
+    const getBoardList = (clubId) => {
+        requestBoardList(
+            clubId,
+            (res) => {
+                console.log("히히 게시판 조회 발사")
+                console.log(res)
+                if(res.status === httpStatusCode.OK) {
+                    boardList.value = res.data.data;
+                    console.log("******* 게시판에 있는 게시물 전체 조회*******")
+                    console.log(boardList.value)
+                    alert(res.data.status)
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }
+
+    const getBoardDetail = (boardId) => {
+        requestSearchBoard(
+            boardId,
+            (res) => {
+                console.log("히히 게시글 상세 조회 발사")
+                console.log(res)
+                if(res.status === httpStatusCode.OK) {
+                    boardDetail.value = res.data.data;
+                    console.log("*******게시글 단 하나*******")
+                    console.log(boardDetail.value)
+                    alert(res.data.status)
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }
+
+    const createBoard = (data) => {
+        requestCreateBoard(
+            data,
+            (res) => {
+                console.log("히히 게시글 작성 발사")
+                console.log(res)
+                if(res.status === httpStatusCode.OK) {
+                    console.log("*******게시글이 잘 작성 됨*******")
+                    alert(res.data.status)
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }
+
+    const updateBoard = (data) => {
+        requestUpdateBoard(
+            data,
+            (res) => {
+                console.log("히히 게시글 수정 발사")
+                console.log(res)
+                if(res.status === httpStatusCode.OK) {
+                    console.log("*******게시글이 잘 수정 됨*******")
+                    alert(res.data.status)
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }
+
+    const deleteBoard = (data) => {
+        requestDeleteBoard(
+            data,
+            (res) => {
+                console.log("히히 게시글 삭제 발사")
+                console.log(res)
+                if(res.status === httpStatusCode.OK) {
+                    console.log("*******게시글이 잘 삭제 됨*******")
+                    alert(res.data.status)
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }
 
 
     return {
-
+        boardList,boardDetail,
+        getBoardList ,getBoardDetail,createBoard,updateBoard,deleteBoard
     }
 })
