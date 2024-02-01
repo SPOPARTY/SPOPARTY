3e8a1ce24e429f7e5758a5c5d9f353ce2a1eb6e4
장승호 <jason9865@naver.com>
2024-02-01 23:58:44 +0900

    [Chore] 추억아카이브, 게시글 수정 로직 개선

--- frontend/src/stores/club/boards.js
+++ frontend/src/stores/club/boards.js
@@ -15,12 +15,9 @@ export const useBoardStore = defineStore("board",() => {
         requestBoardList(
             clubId,
             (res) => {
-                console.log("히히 게시판 조회 발사")
-                console.log(res)
+                // console.log(res)
                 if(res.status === httpStatusCode.OK) {
                     boardList.value = res.data.data;
-                    console.log("******* 게시판에 있는 게시물 전체 조회*******")
-                    console.log(boardList.value)
                 }
             },
             (error) => {
@@ -33,13 +30,10 @@ export const useBoardStore = defineStore("board",() => {
         requestSearchBoard(
             boardId,
             (res) => {
-                console.log("히히 게시글 상세 조회 발사")
-                console.log(res)
+                // console.log("히히 게시글 상세 조회 발사")
+                // console.log(res)
                 if(res.status === httpStatusCode.OK) {
                     boardDetail.value = res.data.data;
-                    console.log("*******게시글 단 하나*******")
-                    console.log(boardDetail.value)
-                    alert(res.data.status)
                 }
             },
             (error) => {
@@ -57,7 +51,7 @@ export const useBoardStore = defineStore("board",() => {
                 if(res.data.status === httpStatusCode.CREATE) {
                     console.log("*******게시글이 잘 작성 됨*******")
                     console.log(res.data.data)
-                    console.log(res.data.data["updated_time"])
+                    // console.log(res.data.data["updated_time"])
                     const clubId = res.data.data["club_id"] // dataForm이므로 data.get()을 써야
                     getBoardList(clubId);
                     alert("게시글 작성 완료")
@@ -74,17 +68,15 @@ export const useBoardStore = defineStore("board",() => {
         requestUpdateBoard(
             data,
             (res) => {
-                console.log("히히 게시글 수정 발사")
-                console.log(res)
-                console.log(res.status)
                 if(res.status === httpStatusCode.OK) {
-                    console.log("*******게시글이 잘 수정 됨*******")
-                    console.log(res.data);
+                    // console.log("*******게시글이 잘 수정 됨*******")
+                    // console.log(res.data);
                     const clubId = res.data.data["club_id"];
-                    console.log("clubId가 잘 뜨나??? ->", clubId)
+                    const boardId = res.data.data["id"];
                     getBoardList(clubId);
+                    getBoardDetail(boardId);
                     alert("게시글 수정 완료")
-                    window.location.reload(`/club/${clubId}/board`)
+                    // window.location.reload(`/club/${clubId}/board`)
                 }
             },
             (error) => {
@@ -99,10 +91,10 @@ export const useBoardStore = defineStore("board",() => {
         requestDeleteBoard(
             boardId,
             (res) => {
-                console.log("히히 게시글 삭제 발사")
-                console.log(res)
+                // console.log("히히 게시글 삭제 발사")
+                // console.log(res)
                 if(res.status === httpStatusCode.OK) {
-                    console.log("*******게시글이 잘 삭제 됨*******")
+                    // console.log("*******게시글이 잘 삭제 됨*******")
                     alert("게시글 삭제 완료")
                     getBoardList(clubId);
                 }
