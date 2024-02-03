72f5ff6637f5de2b1b28771954122d023748bc91
황인규 <rlawldbs42@gmail.com>
2024-02-03 18:12:23 +0900

    Revert "Merge branch 'revert-4ce20cc6' into 'frontend'"
    This reverts merge request !161

--- frontend/src/components/match/MatchList.vue
+++ frontend/src/components/match/MatchList.vue
@@ -40,7 +40,7 @@
                 </v-card>
             </v-col>
         </v-row>
-        <v-row v-if="matches || matches.length === 0">
+        <v-row v-if="matches.length === 0">
             <v-col cols="12" align="center">
             <v-card class="card-match mt-6">
                 <span>예정된 경기가 없습니다!</span>
