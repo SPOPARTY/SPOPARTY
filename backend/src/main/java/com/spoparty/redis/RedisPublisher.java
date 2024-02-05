11303d920e7c23e1f503db91a2ec63e09b713b85
조은별 <joeunbyeol98@gmail.com>
2024-02-05 18:01:17 +0900

    rename: 채팅 DTO 파일 위치 변경

--- backend/src/main/java/com/spoparty/redis/RedisPublisher.java
+++ backend/src/main/java/com/spoparty/redis/RedisPublisher.java
@@ -1,6 +1,6 @@
 package com.spoparty.redis;
 
-import com.spoparty.api.party.dto.ChatRequestDto;
+import com.spoparty.api.party.dto.request.ChatRequestDto;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
