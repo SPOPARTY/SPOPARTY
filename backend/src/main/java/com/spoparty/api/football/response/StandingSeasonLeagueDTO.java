fb62945cf372d7c7f34ddc018333584d0d465268
이효리 <gyfl5542@naver.com>
2024-02-08 15:09:58 +0900

    feat: ?

--- backend/src/main/java/com/spoparty/api/football/response/StandingSeasonLeagueDTO.java
+++ backend/src/main/java/com/spoparty/api/football/response/StandingSeasonLeagueDTO.java
@@ -1,2 +1,26 @@
-package com.spoparty.api.football.response;public class StandingSeasonLeagueDTO {
+package com.spoparty.api.football.response;
+
+import org.springframework.data.jpa.repository.Query;
+
+import com.querydsl.core.annotations.QueryProjection;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class StandingSeasonLeagueDTO {
+	private String season;
+	private Long season_id;
+	private Long league_id;
+	private Long season_league_id;
+
+	@QueryProjection
+
+	public StandingSeasonLeagueDTO(String season, Long season_id, Long league_id, Long season_league_id) {
+		this.season = season;
+		this.season_id = season_id;
+		this.league_id = league_id;
+		this.season_league_id = season_league_id;
+	}
 }
