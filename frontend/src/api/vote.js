dedffc4b40816bdc3efb0303b8b48bd319f78b53
Seung Ho Jang <jason9865@naver.com>
2024-02-12 22:11:11 +0900

    [Fix] conflict 수정

--- frontend/src/api/vote.js
+++ frontend/src/api/vote.js
@@ -1,5 +1,4 @@
 import { localAxios } from '@/util/http-commons'
-import { localAxios } from '@/util/http-commons'
 import Stomp from 'webstomp-client'
 import SockJS from 'sockjs-client'
 import {useVoteStore} from '@/stores/club/party/votes'
@@ -49,16 +48,6 @@ const voteConnect = (partyId) => {
   }
 }
 
-// 투표 생성
-function createVote(data) {
-  // 투표 생성 호출 메서드
-  console.log(stompClient)
-  stompClient.send(
-    '/vote/create',
-    JSON.stringify(data), // body
-    {}, // headers
-  )
-}
 
 function createVote(data) {
   // 투표 생성 호출 메서드
@@ -79,14 +68,6 @@ function doVote(data) {
     {}, // header
   )
 }
-function doVote(data) {
-  // 투표 진행 호출 메서드
-  stompClient.send(
-    '/vote/do',
-    JSON.stringify(data), // body
-    {}, // header
-  )
-}
 
 // 투표 종료(정답, 집계)
 function finishVote() {
@@ -97,30 +78,16 @@ function finishVote() {
     {}, // header
   )
 }
-
-function finishVote() {
-  // 투표 종료 호출 메서드
-  stompClient.send(
-    '/vote/counting',
-    JSON.stringify(data), // body
-    {}, // header
-  )
-}
-
 // 연결 해제
 const voteDisconnect = () => {
   stompClient.disconnect(() => {
     console.log('stomp client disconnected.')
   })
-}
-  stompClient.disconnect(() => {
-    console.log('stomp client disconnected.')
-  })
+
 }
 
 // 투표 상세 조회
 function requestVoteDetail(partyId, voteId, success, fail) {
-  local.get(`/party/${partyId}/votes/${voteId}`).then(success).catch(fail)
   local.get(`/party/${partyId}/votes/${voteId}`).then(success).catch(fail)
 }
 
@@ -128,12 +95,10 @@ function requestVoteDetail(partyId, voteId, success, fail) {
 function requestOngoingVoteList(partyId, success, fail) {
   local.get(`/party/${partyId}/votes/ongoing`).then(success).catch(fail)
 }
-  local.get(`/party/${partyId}/votes/ongoing`).then(success).catch(fail)
-}
+
 
 // 종료된 투표 조회
 function requestFinishedVoteList(partyId, success, fail) {
-  local.get(`/party/${partyId}/votes/finished`).then(success).catch(fail)
   local.get(`/party/${partyId}/votes/finished`).then(success).catch(fail)
 }
 
@@ -143,22 +108,9 @@ function requestMyVoteList(partyId, memberId, success, fail) {
     .get(`/party/${partyId}/votes/members/${memberId}`)
     .then(success)
     .catch(fail)
-  local
-    .get(`/party/${partyId}/votes/members/${memberId}`)
-    .then(success)
-    .catch(fail)
 }
 
 export {
-  voteConnect,
-  createVote,
-  doVote,
-  finishVote,
-  voteDisconnect,
-  requestVoteDetail,
-  requestOngoingVoteList,
-  requestFinishedVoteList,
-  requestMyVoteList,
   voteConnect,
   createVote,
   doVote,
