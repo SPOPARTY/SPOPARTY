3475d1aa6e85b1d9d547e301f37a8c3f37c77113
Seung Ho Jang <jason9865@naver.com>
2024-02-14 23:42:44 +0900

    [Fix] 추억 아카이브 이미지 다운로드 버그 수정

--- frontend/src/components/archive/ArchiveDetail.vue
+++ frontend/src/components/archive/ArchiveDetail.vue
@@ -32,6 +32,10 @@
                         @click="downloadFile(props.detail.file.url)">
                         다운로드
                 </v-btn>
+                <v-btn v-else color="blue darken-1"
+                        @click="downloadImage(props.detail.thumbnail.url)">
+                        다운로드 
+                </v-btn>
                 <v-btn v-if="props.detail.member.id == memberId" color="red darken-2" text @click="deleteProcess(props.detail.id)">삭제</v-btn>
             </v-card-actions>
         </v-card>
@@ -75,6 +79,21 @@ const downloadFile = async (url) => {
     const link = document.createElement('a')
     link.href = downloadUrl
     link.download = 'video.mp4'
+    link.click()
+}
+
+const downloadImage = async (url) => {
+    // console.log(url)
+    // 1. fetch 실행이 끝나면 FETCH API는 내부적으로 Body Object를 상속받아 Response 인스턴스를 생성
+    const res = await fetch(url)
+    // 2. blob() 메소드는 Body Object의 메서드로 상속이 되어있으므로 res.blob() 가능, blob 인스턴스 반환
+    const blob = await res.blob()
+    // 3. 여기서 이 작업을 해주지않으면 link.download에 있는 파일명으로 다운로드하지 못한다.
+    // createObjectURL()는 URL을 DOMString으로 반환한다. (URL 해제는 revokeObjectURL())
+    const downloadUrl = window.URL.createObjectURL(blob) // 이 과정이 필요하다.
+    const link = document.createElement('a')
+    link.href = downloadUrl
+    link.download = 'image.jpg'
     link.click()
 }
 
